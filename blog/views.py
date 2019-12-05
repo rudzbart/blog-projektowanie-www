a558245d7a5049e960d9031894a576e99a39b573
rudzbart <rudzbartosz97@gmail.com>
2019-12-05 15:50:35 +0100

    Laborka 4.

--- blog/views.py
+++ blog/views.py
@@ -43,10 +43,5 @@ def post_edit(request, pk):
 
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        post.delete()
-        return redirect('post_list')
-    else:
-        context = {}
-        context['post'] = post
-        return render(request, 'blog/post_delete.html', context)
+    post.delete()
+    return redirect('post_list')
